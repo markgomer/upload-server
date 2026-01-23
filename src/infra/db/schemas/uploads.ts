8939fddbe223dcfcbf935ccdf4babd8bfee6eff3
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-01-23 16:01:29 -0300

    change to uuidv7

--- src/infra/db/schemas/uploads.ts
+++ src/infra/db/schemas/uploads.ts
@@ -1,8 +1,8 @@
-import { randomUUID } from "node:crypto";
+import { uuidv7 } from "uuidv7";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const uploads = pgTable("uploads", {
-  id: text("id").primaryKey().$defaultFn(() => randomUUID()),
+  id: text("id").primaryKey().$defaultFn(() => uuidv7()),
   name: text("name").notNull(),
   remoteKey: text("remote_key").notNull().unique(),
   remoteUrl: text("remote_url").notNull(),
