9132fb9b2ca33cbcc53e24947adf7e2681a2a117
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-02-10 10:39:57 -0300

    tests: export-uploads function

--- src/app/functions/export-uploads.spec.ts
+++ src/app/functions/export-uploads.spec.ts
@@ -1,13 +1,20 @@
-import { randomUUID } from 'node:crypto'
-import { exportUploads } from '@/app/functions/export-uploads'
-import { getUploads } from '@/app/functions/get-uploads'
-import { isRight, unwrapEither } from '@/infra/shared/either'
-import { makeUpload } from '@/test/factories/make-upload'
-import dayjs from 'dayjs'
-import { describe, expect, it } from 'vitest'
+import { randomUUID } from "node:crypto"
+import { describe, expect, it, spyOn } from "bun:test"
+import { exportUploads } from "@/app/functions/export-uploads"
+import { makeUpload } from "@/test/factories/make-uploads"
+import * as upload from "@/infra/storage/upload-file-to-storage"
+import { isRight, unwrapEither } from "@/infra/shared/either"
 
-describe('export uploads', () => {
-  it('should be able to export uploads', async () => {
+describe("export uploads", () => {
+  it("should be able to export uploads", async () => {
+    const uploadstub = spyOn(upload, "uploadFileToStorage")
+      .mockImplementationOnce( async () => {
+        return {
+          key: `${randomUUID()}.csv`,
+          url: "http://example.file.csv"
+        }
+    });
+
     const namePattern = randomUUID()
 
     const upload1 = await makeUpload({ name: `${namePattern}.wep` })
@@ -19,5 +26,36 @@ describe('export uploads', () => {
     const sut = await exportUploads({
       searchQuery: namePattern,
     })
+
+    const generatedCSVStream = uploadstub.mock.calls[0][0].contentStream
+
+    const csvAsString = await new Promise<String>( (resolve, reject) => {
+      const chunks: Buffer[] = []
+
+      generatedCSVStream.on("data", (chunk: Buffer) => {
+        chunks.push(chunk)
+      })
+
+      generatedCSVStream.on("end", () => {
+        resolve(Buffer.concat(chunks).toString("utf-8"))
+      })
+
+      generatedCSVStream.on("error", (err) => {
+        reject(err)
+      })
+    })
+
+    const csvAsArray = csvAsString.trim().split('\n').map(row => row.split(','))
+
+    expect(isRight(sut)).toBe(true)
+    expect(unwrapEither(sut).reportUrl).toBe("http://example.file.csv")
+    expect(csvAsArray).toEqual([
+      ['ID', 'Name', 'URL', 'Uploaded at'],
+      [upload1!.id, upload1!.name, upload1!.remoteUrl, expect.any(String)],
+      [upload2!.id, upload2!.name, upload2!.remoteUrl, expect.any(String)],
+      [upload3!.id, upload3!.name, upload3!.remoteUrl, expect.any(String)],
+      [upload4!.id, upload4!.name, upload4!.remoteUrl, expect.any(String)],
+      [upload5!.id, upload5!.name, upload5!.remoteUrl, expect.any(String)],
+    ])
   })
 })
